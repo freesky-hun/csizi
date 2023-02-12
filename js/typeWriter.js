960f738cf968a71938d32584a781caf2eabe3543
freesky-hun <kisgabor.private@protonmail.com>
2023-02-12 17:13:14 +0100

    Updated typeWriter. It rewrites the logo text every five seconds.

--- js/typeWriter.js
+++ js/typeWriter.js
@@ -1,13 +1,30 @@
 var i = 0;
 var txt = "Ön megálmodja, én megvalósítom!";
-var speed = 100;
             
 function typeWriter() {
     if (i < txt.length) {
         document.getElementById("text").innerHTML += txt.charAt(i);
         i++;
-        setTimeout(typeWriter, speed);
+        setTimeout(typeWriter, 50);
     }
 }
 
 typeWriter();
+var k = 0;      
+
+function myLoop() {
+  setTimeout(function() {
+
+    document.getElementById("text").innerHTML = "";
+    i = 0;
+
+    typeWriter();
+
+    if (k < 1) {
+      myLoop();
+    }
+    
+  }, 5000)
+}
+
+myLoop();
